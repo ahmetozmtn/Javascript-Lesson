23eb1d4ac703ef46020f977cc15af9adda4ec44b
ahmetozmtn <ahmethack13@gmial.com>
2023-12-30 16:45:27 +0300

    conditions finished

---
+++ Basic/006-conditions/script.js
@@ -0,0 +1,12 @@
+let username = "ahmetozmtn";
+let password = "12345";
+
+if (username == "ahmetozmtn") {
+    if (password == "12345") {
+        console.log(`${username} logged in`);
+    } else {
+        console.log("password is wrong");
+    }
+} else {
+    console.log("username is wrong");
+}
