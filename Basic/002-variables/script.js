1af987e5b53dfedcc3dd271e7696c7b4cb8cb07c
ahmetozmtn <ahmethack13@gmial.com>
2023-12-30 14:14:43 +0300

    variables

--- Basic/002-variables/script.js
+++ Basic/002-variables/script.js
@@ -2,9 +2,11 @@ var maasAli = 5000;
 var maasCan = 6000;
 var zam = 0.35;
 
+var msg = "Hello World!";
 // Türkçe karakter kullanmamalıyız
 // değişkenler sayı ile başlayamaz.
 // Arada boşluk olmaz.
 
 console.log(maasAli + maasAli * zam);
 console.log(maasCan + maasCan * zam);
+console.log(msg);
