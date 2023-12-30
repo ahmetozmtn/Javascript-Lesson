cdd2874941860aaaa8315d9e2c59f4f190f51fef
ahmetozmtn <ahmethack13@gmial.com>
2023-12-30 16:23:08 +0300

    operators

---
+++ Basic/005-operators/script.js
@@ -0,0 +1,36 @@
+let sonuc;
+let a = 10,
+    b = 20,
+    c = 32;
+
+// 1- Aritmetik Operatörler
+sonuc = a + b;
+sonuc = a - b;
+sonuc = a * b;
+sonuc = a / b;
+sonuc = a % b;
+// sonuc = a++; // a + 1
+// sonuc = ++a; // a + 1
+// sonuc = --a; // a - 1
+// sonuc = a--; // a + 1
+
+// 2- Atama Operatörler
+sonuc = a;
+sonuc += a; // sonuc = sonuc + a
+sonuc -= a; // sonuc = sonuc - a
+sonuc *= a; // sonuc = sonuc * a
+sonuc /= a; // sonuc = sonuc / a
+sonuc %= a; // sonuc = sonuc % a
+
+// 3- Karşılaştırma Operatörler
+sonuc = a == b;
+sonuc = a != b;
+sonuc = a === b;
+sonuc = a > b;
+sonuc = a >= b;
+sonuc = a < b;
+sonuc = a <= b;
+
+// 4- Mantıksal Operatörler
+
+console.log(sonuc);
