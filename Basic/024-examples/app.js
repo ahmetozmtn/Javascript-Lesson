e5c385fad992c64a238953cd944aa21f37010a66
ahmetozmtn <ahmethack13@gmial.com>
2024-01-05 14:12:48 +0300

    example added

--- Basic/024-examples/app.js
+++ Basic/024-examples/app.js
@@ -1,11 +1,18 @@
-let vize1 = prompt("1. Vize Notunuzu Girin: ");
-let vize2 = prompt("1. Vize Notunuzu Girin: ");
-let final = prompt("Final Notunuzu Girin: ");
+// let vize1 = prompt("1. Vize Notunuzu Girin: ");
+// let vize2 = prompt("1. Vize Notunuzu Girin: ");
+// let final = prompt("Final Notunuzu Girin: ");
 
-let ortalama = vize1 * 0.3 + vize2 * 0.3 + final * 0.4;
+// let ortalama = vize1 * 0.3 + vize2 * 0.3 + final * 0.4;
 
-if (ortalama > 60 && final > 60) {
-    alert("Geçtiniz!");
-} else {
-    alert("Kaldınız");
+// if (ortalama > 60 && final > 60) {
+//     alert("Geçtiniz!");
+// } else {
+//     alert("Kaldınız");
+// }
+
+for (let first = 1; first <= 10; first++) {
+    for (let second = 1; second <= 10; second++) {
+        console.log(`${first}x${second}=${first * second}`);
+    }
+    console.log("-----------------------");
 }
