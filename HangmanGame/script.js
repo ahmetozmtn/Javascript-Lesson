2fbb5860e4f49bebd1751a14c3795f6e69d63f62
ahmetozmtn <ahmethack13@gmial.com>
2024-07-03 16:34:47 +0300

    keydown event

--- HangmanGame/script.js
+++ HangmanGame/script.js
@@ -1,7 +1,10 @@
 const wordElement = document.getElementById("word");
+const popup = document.getElementById("popup-container");
+const messageElement = document.getElementById("success-message");
 
 const correctLetters = ["h", "t", "m", "l", "o", "e"];
 const wrongLetters = [];
+const selectedWord = getRandomWord();
 
 function getRandomWord() {
     const words = [
@@ -16,11 +19,7 @@ function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)];
 }
 
-console.log(getRandomWord());
-
 function displayWord() {
-    const selectedWord = getRandomWord();
-
     wordElement.innerHTML = `
         ${selectedWord
             .split("")
@@ -37,8 +36,29 @@ function displayWord() {
 
     const w = wordElement.innerText.replace(/\n/g, "");
     if (w === selectedWord) {
-        console.log("You win");
+        popup.style.display = "flex";
+        messageElement.innerText = "You Won!";
     }
 }
+
+window.addEventListener("keydown", (e) => {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+                displayWord();
+            } else {
+                console.log("bu harfi zaten kullandınız");
+            }
+        } else {
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+                console.log("hatalı harfleri güncelle");
+            }
+        }
+    }
+});
 
 displayWord();
