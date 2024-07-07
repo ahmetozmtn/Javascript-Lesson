7493f4fb69be00512a16e8c4199fb9bb18130107
ahmetozmtn <ahmethack13@gmial.com>
2024-07-07 22:13:26 +0300

    spread

--- 11-Modern Javascript - ES6|ES7/script.js
+++ 11-Modern Javascript - ES6|ES7/script.js
@@ -23,26 +23,26 @@
 
 // console.log(totalES6(10, 20));
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const products = [
-    {
-        name: "Laptop",
-        price: 10000,
-    },
-    {
-        name: "Mouse",
-        price: 500,
-    },
-    {
-        name: "Keyboard",
-        price: 1000,
-    },
-    {
-        name: "Monitor",
-        price: 2000,
-    },
-];
+// const products = [
+//     {
+//         name: "Laptop",
+//         price: 10000,
+//     },
+//     {
+//         name: "Mouse",
+//         price: 500,
+//     },
+//     {
+//         name: "Keyboard",
+//         price: 1000,
+//     },
+//     {
+//         name: "Monitor",
+//         price: 2000,
+//     },
+// ];
 
 // const tekSayilar = numbers.filter((number) => number % 2 === 1);
 // const tekSayilar = numbers.map((number) => number % 2 === 1);
@@ -52,6 +52,31 @@ const products = [
 //     .map((product) => product.name);
 
 // result = products.find((product) => product.price >= 1000);
-result = products.findIndex((product) => product.price >= 1000);
+// result = products.findIndex((product) => product.price >= 1000);
 
-console.log(result);
+// console.log(result);
+
+// array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log(...array);
+
+// array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// array2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// const result = [...array1, ...array2];
+
+// console.log(result);
+
+// rest parameters
+
+// function sum(...numbers) {
+//     return numbers.reduce((total, number) => total + number, 0);
+// }
+
+// sum(1, 2, 3, 4, 5);
+
+function showInfo(name, age, ...country) {
+    console.log(name, age, country);
+}
+
+showInfo("Andrian", 25, "Turkey", "USA", "Germany");
